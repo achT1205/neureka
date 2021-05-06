82d100c2c4b509fcd0aa360777af2b6c8c69e546
achT1205 <achilletuglo12@gmail.com>
2021-05-06 14:12:59 +0200

    commit

--- NeurekaUi/src/views/patients/PatientSchedules.vue
+++ NeurekaUi/src/views/patients/PatientSchedules.vue
@@ -105,7 +105,11 @@
             </v-tabs-items>
           </template>
           <template v-else>
-            <v-alert color="primary text-center" class="mb-0" style="color: white">
+            <v-alert
+              color="primary text-center"
+              class="mb-0"
+              style="color: white"
+            >
               There is currently no visit for
               <strong v-if="patient"
                 >{{ patient.firstName }} {{ patient.lastName }}
